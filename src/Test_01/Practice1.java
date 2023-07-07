3eba49f699b077068f5e5b9e6c6e04514baba5e8
nilgün yılmaz çınar <temiriqoenilgun@gmail.com>
2023-07-07 13:32:30 +0300

    added hello

--- src/Test_01/Practice1.java
+++ src/Test_01/Practice1.java
@@ -2,6 +2,8 @@ package Test_01;
 
 public class Practice1 {
     public static void main(String[] args) {
-        System.out.println("hello merve");
+        System.out.println("hello");
     }
+
+
 }
